df295a02ebe45947e981737094a772282fab5486
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-04-30 15:19:04 -0300

    update

--- fausto/src/pages/Inicial.jsx
+++ fausto/src/pages/Inicial.jsx
@@ -3,7 +3,8 @@
 export default function Inicial() {
   return (
     <div>
-      <h1 className="title">Seja bem vindo(a) a minha API :)</h1>
+      <h1 className="title">Seja bem vindo(a) a minha API 👩‍💻</h1>
+      <p className="conteudo">Projeto foi realizado durante a matéria de desenvolvimento frontend. Metodologia aplicada foi o criação de uma simples aplicação em React! </p>
       <p className="conteudo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis doloribus dolore ratione vitae praesentium totam itaque. Iste dignissimos a blanditiis. Qui facere non dicta dolorum quaerat veritatis fugiat rerum voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus illo esse numquam ullam natus nobis laborum ex dolor voluptatem fugiat voluptates culpa laudantium, asperiores doloribus minima fugit? Deleniti, nesciunt cum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit quam dicta id voluptas iste praesentium deleniti esse assumenda. Dolores labore accusantium repellendus saepe sunt ratione non autem quibusdam doloremque reiciendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus at odio sunt dignissimos quibusdam natus quaerat consectetur omnis id, illo quasi obcaecati temporibus tempore suscipit! Animi sed debitis at culpa.</p>
     </div>
   );
